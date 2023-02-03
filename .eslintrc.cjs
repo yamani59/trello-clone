151cc8bf00cb3987173efc624f1981ca79e45dc7
yamani59 <yamaniyud@gmail.com>
2023-02-03 12:01:12 +0700

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
